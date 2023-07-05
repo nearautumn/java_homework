de4b00830f36c11a54f731f1457352147d7ef2df
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-07-05 21:05:44 +0300

    homework 2 finished without task 3

--- homework2/src/main/java/by/aleksandrov/api/homework2/Program.java
+++ homework2/src/main/java/by/aleksandrov/api/homework2/Program.java
@@ -1,21 +1,23 @@
 package by.aleksandrov.api.homework2;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
+//import java.util.logging.FileHandler;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import java.util.logging.SimpleFormatter;
 
 public class Program {
 
-    public static void main(String[] args) throws IOException {
+            public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Выберите задание: ");
         System.out.println("1 - Задача 1");
         System.out.println("2 - Задача 2");
-        System.out.println("3 - Задача 3");
+//        System.out.println("3 - Задача 3");
         int userChoice = Integer.parseInt(sc.nextLine());
 
         switch (userChoice) {
@@ -25,7 +27,7 @@ public class Program {
             case 2:
                 task2();
                 break;
-            case 3:
+//            case 3:
 //                task3();
 //                break;
             default:
@@ -61,7 +63,7 @@ public class Program {
         }
     }
 
-    static void task2() throws IOException {
+    static void task2() {
 
         System.out.println("Сортировка пузырьком с записью в лог...");
 
@@ -73,27 +75,84 @@ public class Program {
         //System.out.println(Arrays.toString(numbers));
         String out = Arrays.toString(numbers) + '\n';
 
-        File log = new File("log_task2.txt");
-        FileWriter fw = new FileWriter(log, true);
-        fw.write("Initial array: ");
-        fw.write(out);
+        try {
+            File log = new File("log_task2.txt");
+            FileWriter fw = new FileWriter(log, true);
 
-        double temp;
+            fw.write("Initial array: ");
+            fw.write(out);
 
-        fw.write("Sorting... \n");
-        for (int i = 0; i < numbers.length-1; i++ ) {
-            for (int j = numbers.length - 1; j > i;  j--) {
-                if (numbers[j - 1] > numbers[j]) {
-                    temp = numbers[j - 1];
-                    numbers[j - 1] = numbers[j];
-                    numbers[j] = temp;
-                    out = Arrays.toString(numbers) + '\n';
-                    fw.write(out);
+            double temp;
+
+            fw.write("Sorting... \n");
+            for (int i = 0; i < numbers.length - 1; i++) {
+                for (int j = numbers.length - 1; j > i; j--) {
+                    if (numbers[j - 1] > numbers[j]) {
+                        temp = numbers[j - 1];
+                        numbers[j - 1] = numbers[j];
+                        numbers[j] = temp;
+                        out = Arrays.toString(numbers) + '\n';
+                        fw.write(out);
+                    }
                 }
             }
+            fw.close();
+            System.out.println("Файл записан!");
         }
-        fw.close();
-        System.out.println("Файл записан!");
+        catch (IOException ex) {
+            System.out.println("Ошибка записи файла!");
+        }
         //System.out.println(Arrays.toString(numbers));
     }
+
+//    static void task3() {
+//
+//        /**
+//         * 3) Дана json-строка (можно сохранить в файл и читать из файла)
+//         * [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+//         * Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+//         * Пример вывода:
+//         * Студент Иванов получил 5 по предмету Математика.
+//         * Студент Петрова получил 4 по предмету Информатика.
+//         * Студент Краснов получил 5 по предмету Физика.
+//         */
+//
+//        Logger logger = Logger.getLogger(Program.class.getName());
+//
+//        try {
+//            FileHandler fh = new FileHandler("task3_log.txt");
+//            logger.addHandler(fh);
+//            FileReader fr = new FileReader("task3.txt");
+//            Scanner sc = new Scanner(fr);
+//            SimpleFormatter sFormat = new SimpleFormatter();
+//            fh.setFormatter(sFormat);
+//
+//
+//            String input = sc.nextLine();
+//
+//            StringBuilder sb = new StringBuilder(input);
+//            sb.
+//
+//            fr.close();
+//            logger.log(Level.INFO, "task3.txt успешно загружен!");
+//
+//            input = input.substring(input.indexOf("[")+1, input.indexOf("]"));
+//            String[] inputArr = input.;
+//            for (int i = 0; i < inputArr.length; i++) {
+//                System.out.println(inputArr[i]);
+//            }
+//
+//        }
+//        catch (FileNotFoundException task3NotFound) {
+//            System.out.println("Ошибка чтения файла task3.txt!");
+//            logger.log(Level.WARNING, "Ошибка чтения файла task3.txt!");
+//        }
+//        catch (SecurityException sEx) {
+//            System.out.println("Ошибка прав доступа!"); // на самом деле я пока не очень понял смысл этого исключения :)
+//        }
+//        catch (IOException ioExc) {
+//            System.out.println("Ошибка доступа к файлу task3_log.txt!");
+//        }
+//
+//    }
 }
